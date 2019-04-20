b50a29a23c89179524d5bbdb06d28dfee31c898d
Kevin <stradtkt22@gmail.com>
2019-04-19 20:48:37 -0400

    Made app into express app

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require('path');
 const port = 8000;
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}))
 app.set("views", path.join(__dirname, "./views"));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+
+app.use(require('./server/routes/routes'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', () => {
+    console.log(`App is running on port ${port}`);
+});
